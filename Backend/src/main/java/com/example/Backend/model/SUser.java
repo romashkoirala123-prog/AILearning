b146009fe20be7217226a1ccb0060db7813db094
Romash Koirala <romashkoirala123@gmail.com>
2026-06-07 08:10:56 +0545

    Adding Backend to project

---
+++ Backend/src/main/java/com/example/Backend/model/SUser.java
@@ -0,0 +1,23 @@
+package com.example.Backend.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Document(collection = "users")
+@Getter
+@Setter
+public class SUser {
+    @Id
+    private String id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+
+    public SUser() {
+    }
+}
