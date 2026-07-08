a9db4de58625caaefb89800b094e6c7545fad37f
Romash Koirala <romashkoirala123@gmail.com>
2026-07-08 18:06:49 +0545

    reviewd repo layer

--- backend/src/main/java/com/example/Backend/repository/userRepository.java
+++ backend/src/main/java/com/example/Backend/repository/userRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 @Repository
-public interface userRepository extends MongoRepository<SUser,Long> {
+public interface userRepository extends MongoRepository<SUser,String> {
     Optional<SUser>  findByUsername(String username);
     List<SUser>  findByEmail(String email);
 }
