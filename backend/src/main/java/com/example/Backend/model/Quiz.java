fad0d06a1225b80760e2e4ec83b69e83dfb95312
Romash Koirala <romashkoirala123@gmail.com>
2026-07-12 12:04:12 +0545

    removed unwanted imports

--- backend/src/main/java/com/example/Backend/model/Quiz.java
+++ backend/src/main/java/com/example/Backend/model/Quiz.java
@@ -6,7 +6,6 @@ import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.mongodb.core.index.CompoundIndexes;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
